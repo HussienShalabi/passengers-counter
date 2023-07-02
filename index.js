36c1eb9e9c4ea602686738485321c579eb65755f
HussienShalabi <hsonshb@gmail.com>
2023-07-02 22:17:03 +0300

    saving

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 let count = 0
 let counter = document.getElementById("counter-el")
+let savings = document.getElementById('savings-el')
 function increment(){
-    count = count + 1 
+    count +=  1 
     counter.innerText = count
 }
 
 function save(){
     console.log(count)
+    let saveCount = count + ','
+    savings.innerText += saveCount
     count = 0
     counter.innerText = count
+    
+
 }
 
-function decrement(){
-    count = count - 1
+function clean(){
+    count =  0
     counter.innerText = count
+    savings.innerText = 'previous entries:'
 }
+
+
 
 
 
